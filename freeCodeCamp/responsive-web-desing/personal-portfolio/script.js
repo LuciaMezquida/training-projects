39abda2fc4274375fc43e19675631032b154f2b4
Lucia <lucia.mezquida@gmail.com>
2020-06-23 16:38:45 +0200

    Script  and  style contact added

---
+++ freeCodeCamp/responsive-web-desing/personal-portfolio/script.js
@@ -0,0 +1,8 @@
+function change() {
+    document.getElementById("email").innerHTML = "lucia.mezquida@gmail.com";
+}
+
+function noChange() {
+    document.getElementById("email").innerHTML = "Email";
+}
+    
