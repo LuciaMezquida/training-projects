237b362dffb4145e723d15013b1501e3b9d7e768
Lucia <lucia.mezquida@gmail.com>
2020-07-14 18:08:27 +0200

    Modifying an event and listening to keyboard

---
+++ vue-course/script.js
@@ -0,0 +1,27 @@
+new Vue ({
+    el: '#app',
+    data: {
+        counter: 0,
+        x: 0,
+        y: 0,
+        title: 'Hello World!',
+        link: 'http://google.com',
+        finishedLink: '<a href="http://google.com">Google</a>'
+    },
+    methods: {
+        sayHello() {
+            this.title = 'Hello!'
+            return this.title;
+        },
+        increase(n, event) {
+            this.counter += n;
+        },
+        updateCoordinates(event) {
+            this.x = event.clientX;
+            this.y = event.clientY;
+        },
+        alertMe() {
+            alert('Alert!');
+        }
+    }
+});
