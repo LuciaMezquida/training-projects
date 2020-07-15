00480f49a310cc6880ad1d43621b360a2649a4ca
Lucia <lucia.mezquida@gmail.com>
2020-07-15 17:32:02 +0200

    Reacting to changes with computed properties

--- vue-course/script.js
+++ vue-course/script.js
@@ -25,9 +25,29 @@ new Vue ({
         }
     }
 });
+
 new Vue ({
     el: '#app2',
     data: {
         name: 'Max'
+    }
+});
+
+new Vue ({
+    el: '#app3',
+    data: {
+        counter: 0,
+        secondCounter: 0
+    },
+    computed: {
+        output() {
+            return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5;'
+
+        }
+    },
+    methods: {
+        result() {
+            return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5;'
+        }
     }
 });
