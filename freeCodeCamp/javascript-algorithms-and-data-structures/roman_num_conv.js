9a3756cfcf8f50fd39419a843e00648fa6ffd0d8
Lucia <lucia.mezquida@gmail.com>
2020-07-28 10:11:42 +0200

    Roman numeral converter

---
+++ freeCodeCamp/javascript-algorithms-and-data-structures/roman_num_conv.js
@@ -0,0 +1,25 @@
+function convertToRoman(num) {
+    const romanObject = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1
+  };
+  let roman = '';
+  for (let i in romanObject) {
+    while (num >= romanObject[i]) {
+      num = num - romanObject[i];
+      roman = roman + i;
+    }
+  }
+ return roman;
+}
